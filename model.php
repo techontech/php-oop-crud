a8f8d98acfc626a6b5e34d5f5d43ba19ce1345f9
TechOnTech <67923422+techontech@users.noreply.github.com>
2020-07-15 15:46:30 +0530

    Add files via upload

---
+++ model.php
@@ -0,0 +1,108 @@
+<?php 
+
+	Class Model{
+
+		private $server = "localhost";
+		private $username = "root";
+		private $password;
+		private $db = "oop_crud";
+		private $conn;
+
+		public function __construct(){
+			try {
+				
+				$this->conn = new mysqli($this->server,$this->username,$this->password,$this->db);
+			} catch (Exception $e) {
+				echo "connection failed" . $e->getMessage();
+			}
+		}
+
+		public function insert(){
+
+			if (isset($_POST['submit'])) {
+				if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['mobile']) && isset($_POST['address'])) {
+					if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['mobile']) && !empty($_POST['address']) ) {
+						
+						$name = $_POST['name'];
+						$mobile = $_POST['mobile'];
+						$email = $_POST['email'];
+						$address = $_POST['address'];
+
+						$query = "INSERT INTO records (name,email,mobile,address) VALUES ('$name','$email','$mobile','$address')";
+						if ($sql = $this->conn->query($query)) {
+							echo "<script>alert('records added successfully');</script>";
+							echo "<script>window.location.href = 'index.php';</script>";
+						}else{
+							echo "<script>alert('failed');</script>";
+							echo "<script>window.location.href = 'index.php';</script>";
+						}
+
+					}else{
+						echo "<script>alert('empty');</script>";
+						echo "<script>window.location.href = 'index.php';</script>";
+					}
+				}
+			}
+		}
+
+		public function fetch(){
+			$data = null;
+
+			$query = "SELECT * FROM records";
+			if ($sql = $this->conn->query($query)) {
+				while ($row = mysqli_fetch_assoc($sql)) {
+					$data[] = $row;
+				}
+			}
+			return $data;
+		}
+
+		public function delete($id){
+
+			$query = "DELETE FROM records where id = '$id'";
+			if ($sql = $this->conn->query($query)) {
+				return true;
+			}else{
+				return false;
+			}
+		}
+
+		public function fetch_single($id){
+
+			$data = null;
+
+			$query = "SELECT * FROM records WHERE id = '$id'";
+			if ($sql = $this->conn->query($query)) {
+				while ($row = $sql->fetch_assoc()) {
+					$data = $row;
+				}
+			}
+			return $data;
+		}
+
+		public function edit($id){
+
+			$data = null;
+
+			$query = "SELECT * FROM records WHERE id = '$id'";
+			if ($sql = $this->conn->query($query)) {
+				while($row = $sql->fetch_assoc()){
+					$data = $row;
+				}
+			}
+			return $data;
+		}
+
+		public function update($data){
+
+			$query = "UPDATE records SET name='$data[name]', email='$data[email]', mobile='$data[mobile]', address='$data[address]' WHERE id='$data[id] '";
+
+			if ($sql = $this->conn->query($query)) {
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
+
+ ?>
---
+++ read.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>PHP OOP CRUD TUTORIAL</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 mt-5">
+          <h1 class="text-center">PHP OOP CRUD TUTORIAL - SINGLE RECORD</h1>
+          <hr style="height: 1px;color: black;background-color: black;">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-5 mx-auto">
+          <?php
+
+              include 'model.php';
+              $model = new Model();
+              $id = $_REQUEST['id'];
+              $row = $model->fetch_single($id);
+              if(!empty($row)){
+
+          ?>
+          <div class="card">
+            <div class="card-header">
+              Single Record
+            </div>
+            <div class="card-body">
+              <p>Name = <?php echo $row['name']; ?></p>
+              <p>Email = <?php echo $row['email']; ?></p>
+              <p>Mobile No. = <?php echo $row['mobile']; ?></p>
+              <p>Address = <?php echo $row['address']; ?></p>
+            </div>
+          </div>
+          <?php
+            }else{
+            echo "no data";
+          }
+          ?>
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
